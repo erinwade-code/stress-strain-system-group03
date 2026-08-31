a0448b155c68d8a619fdd9b6c304ece829d373a1
gabrieldolar <gabriel.dolar@ust.edu.ph>
2026-08-31 15:06:47 +0800

    commit calculate

--- task4.py
+++ task4.py
@@ -7,4 +7,8 @@ def calculate_strain(change_in_length: float, original_length: float) -> float:
   if orignal_length <= 0:
     raise ValueError("Original length must be greater than zero.")
   return change_in_length / original_length
-
+  
+def calculate_youngs_modulus(stress: float, strain: float) -> float:
+    if strain <= 0:
+        raise ValueError("Strain must be greater than zero.")
+    return (stress / strain) / 1000.0
