ade598271d29ced2342f8da7d02a51b269137775
gabrieldolar <gabriel.dolar@ust.edu.ph>
2026-08-31 15:40:46 +0800

    fixed typo orgnal -> original

--- task4.py
+++ task4.py
@@ -4,7 +4,7 @@ def calculate_stress(force: float, area: float) -> float:
   return force / area
   
 def calculate_strain(change_in_length: float, original_length: float) -> float:
-  if orignal_length <= 0:
+  if original_length <= 0:
     raise ValueError("Original length must be greater than zero.")
   return change_in_length / original_length
   
