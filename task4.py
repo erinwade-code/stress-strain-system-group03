83ae0da794731415bd6fc63e11fcd89eab4494d8
gabrieldolar <gabriel.dolar@ust.edu.ph>
2026-08-31 15:06:09 +0800

    commit 2

--- task4.py
+++ task4.py
@@ -2,4 +2,9 @@ def calculate_stress(force: float, area: float) -> float:
   if area <= 0: 
     raise ValueError("Cross-sectional area must be greater than zero.")
   return force / area
+  
+def calculate_strain(change_in_length: float, original_length: float) -> float:
+  if orignal_length <= 0:
+    raise ValueError("Original length must be greater than zero.")
+  return change_in_length / original_length
 
